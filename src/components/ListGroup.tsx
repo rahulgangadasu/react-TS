41be9e377e4f4e0e00b8c6da1e7ce2472980672b
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-04-17 16:21:17 -0500

    Add useState Hook and managing state.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,24 @@
-import type { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
-  //Event Handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //State Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1); //useState() returns two array elements [variable, updater function]
   return (
     <>
       <h1>List of Cities</h1>
       {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
@@ -20,15 +28,3 @@ function ListGroup() {
 }
 
 export default ListGroup;
-
-/* -> <h1>List </h1> returning multiple tags is not allowed in React, 
-      so we should wrap them in a <div> tag or import { Fragment } from "react";.
-      or we can use <Fragment> or empty <>(angular brackets).
-*/
-
-/*-> we use curly braces {} to render data dynamically.
-     In JSX we can only use html elements or other react components.
-     For conditional rendering like items.length === 0 ? <p>No items to display</p> : null
-     true && 'something' displays 'something' in console.
-     false && 'nothing' displays false, so we can use && in place of ternary (?:) operator.
- */
