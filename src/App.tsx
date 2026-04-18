4b9714e9166706c1c5f259ab1288616f4701bfbe
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-04-18 12:37:41 -0500

    Add CSS to ListGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import Alert from "./Components/Alert";
 import Button from "./Components/Button";
+import ListGroup from "./Components/ListGroup";
 function App() {
   const [alertVisible, setAlertVisiblity] = useState(false);
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
   return (
     <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={(item) => console.log(item)}
+      />
       {alertVisible && (
-        <Alert onClose={() => setAlertVisiblity(false)}>My Alert</Alert>
+        <Alert onClose={() => setAlertVisiblity(false)}>Thanks! Close!!!</Alert>
       )}
       <Button color="dark" onClick={() => setAlertVisiblity(true)}>
-        My Button
+        Let's Go
       </Button>
     </div>
   );
