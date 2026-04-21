41209ae7a69e0247f79c0c73017f3ed5d2a1abae
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-04-21 17:53:16 -0500

    Add ExpandableText component and create StateUpdate

---
+++ src/components/Excercises/StateUpdate.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const [game, setGame] = useState({
+  id: 1,
+  player: {
+    name: "Rahul",
+  },
+});
+const handleGameClick = () => {
+  setGame({ ...game, player: { ...game.player, name: "Phanindra" } });
+};
+
+const [pizza, setPizza] = useState({
+  name: "Veggie Pizza",
+  toppings: ["jalapeno"],
+});
+const handlePizzaClick = () => {
+  setPizza({ ...pizza, toppings: [...pizza.toppings, "mushroom"] });
+};
+
+const [cart, setCart] = useState({
+  discount: 0.1,
+  items: [
+    {
+      id: 1,
+      item: "Product 1",
+      quantity: 1,
+    },
+    {
+      id: 2,
+      item: "Product 2",
+      quantity: 1,
+    },
+  ],
+});
+const handleCartClick = () => {
+  setCart({
+    ...cart,
+    items: [
+      ...cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item,
+      ),
+    ],
+  });
+};
+
+const StateUpdate = () => {
+  return <div>StateUpdate</div>;
+};
+
+export default StateUpdate;
