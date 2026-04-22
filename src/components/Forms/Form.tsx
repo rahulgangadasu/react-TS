fd92e900bbbb7b439be95310276793021f42bec7
rahulgangadasu <rahulreddygangadasu2709@gmail.com>
2026-04-22 14:03:00 -0500

    Add react-hook-form for form handling and validation in Form component

--- src/components/Forms/Form.tsx
+++ src/components/Forms/Form.tsx
@@ -1,19 +1,58 @@
+/*
+import { useRef } from "react";
+import { useState } from "react";
+*/
+
+import { useForm, type FieldValues } from "react-hook-form";
+
+interface FormData {
+  //to show our input fields in errors of the useForm() hook.
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const formAction = () => console.log("submitted");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }, //nested destructuring
+  } = useForm<FormData>();
+  //const formAction = () => {};
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
 
   return (
-    <form action={formAction}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" name="name" type="text" className="form-control" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          name="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && ( //?. this is optional chaining.
+          <p className="text-danger">This name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" name="age" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          id="age"
+          name="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
@@ -21,3 +60,27 @@ const Form = () => {
 };
 
 export default Form;
+
+/* using useRef()
+<input ref={nameRef}>
+<input ref={ageRef}>
+
+const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { fullname: "", age: 0 };
+
+  const formAction = () => {
+    if (nameRef.current !== null) person.fullname = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
+  };
+  */
+
+/* using useState() 
+<input onChange={(event) =>setPerson({ ...person, age: parseInt(event.target.value) })} value={person.age}>
+<input onChange={(event) =>setPerson({ ...person, age: parseInt(event.target.value) })}value={person.age}>
+ const [person, setPerson] = useState({
+    fullname: "",
+    age: 0,
+  });
+  */
